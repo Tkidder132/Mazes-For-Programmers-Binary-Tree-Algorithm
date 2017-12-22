3c12df5b1d1721558c169e025623b73aec44141e
Tristan <tkidder132@gmail.com>
2017-12-22 01:11:25 -0600

    Base maze placement, still requires reverse door addition

--- Assets/_Scripts/MazeGeneratorController.cs
+++ Assets/_Scripts/MazeGeneratorController.cs
@@ -6,6 +6,8 @@ public class MazeGeneratorController : MonoBehaviour
     public int mazeHeight = 5;
 
     MazeCell[,] generatedMaze;
+    public GameObject[] mazeParts = new GameObject[6];
+    //0, 1, 2 corner, 2 straight, 3, 4
 
 	// Use this for initialization
 	void Start ()
@@ -51,6 +53,7 @@ public class MazeGeneratorController : MonoBehaviour
 
     void CreateMaze()
     {
+        float x = 0.0f, z = 0.0f;
         for(int i = 0; i < mazeWidth; i++)
         {
             for(int j = 0; j < mazeHeight; j++)
@@ -75,60 +78,80 @@ public class MazeGeneratorController : MonoBehaviour
                     doorBinary = doorBinary + generatedMaze[i - i, j].doorRight.ToString();
                 }
 
+                //translated in steps of 6
                 switch(doorBinary)
                 {
                     case "0000":
+                        Instantiate(mazeParts[0], new Vector3(x, 0, z), Quaternion.identity);
                         Debug.Log("No Doors");
                         break;
                     case "0001":
+                        Instantiate(mazeParts[1], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 270, 0)));
                         Debug.Log("1 Door: Left");
                         break;
                     case "0010":
+                        Instantiate(mazeParts[1], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 180, 0)));
                         Debug.Log("1 Door: Down");
                         break;
                     case "0011":
+                        Instantiate(mazeParts[2], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 270, 0)));
                         Debug.Log("2 Doors: Down, Left");
                         break;
                     case "0100":
+                        Instantiate(mazeParts[1], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 90, 0)));
                         Debug.Log("1 Door: Right");
                         break;
                     case "0101":
+                        Instantiate(mazeParts[3], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 90, 0)));
                         Debug.Log("2 Doors: Right, Left");
                         break;
                     case "0110":
+                        Instantiate(mazeParts[2], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 90, 0)));
                         Debug.Log("2 Doors: Right, Down");
                         break;
                     case "0111":
+                        Instantiate(mazeParts[4], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 180, 0)));
                         Debug.Log("3 Doors: Right, Down, Left");
                         break;
                     case "1000":
+                        Instantiate(mazeParts[1], new Vector3(x, 0, z), Quaternion.identity);
                         Debug.Log("1 Door: Up");
                         break;
                     case "1001":
+                        Instantiate(mazeParts[2], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 270, 0)));
                         Debug.Log("2 Doors: Up, Left");
                         break;
                     case "1010":
+                        Instantiate(mazeParts[3], new Vector3(x, 0, z), Quaternion.identity);
                         Debug.Log("2 Doors: Up, Down");
                         break;
                     case "1011":
+                        Instantiate(mazeParts[4], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 270, 0)));
                         Debug.Log("3 Doors: Up, Down, Left");
                         break;
                     case "1100":
+                        Instantiate(mazeParts[2], new Vector3(x, 0, z), Quaternion.identity);
                         Debug.Log("2 Doors: Up, Right");
                         break;
                     case "1101":
+                        Instantiate(mazeParts[4], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 0, 0)));
                         Debug.Log("3 Doors: Up, Right, Left");
                         break;
                     case "1110":
+                        Instantiate(mazeParts[4], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0, 90, 0)));
                         Debug.Log("3 Doors: Up, Right, Down");
                         break;
                     case "1111":
+                        Instantiate(mazeParts[5], new Vector3(x, 0, z), Quaternion.identity);
                         Debug.Log("4 Doors: Up, Right, Down, Left");
                         break;
                     default:
                         break;
                 }
+                z += 6.0f;
             }
+            x += 6.0f;
+            z = 0.0f;
         }
     }
 
